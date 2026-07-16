e2cff5f6feec6355ceca974a6f1e866b03f5ee88
cagedbird043 <cagedbird043@gmail.com>
2026-07-17 07:43:35 +0800

    feat: configure 3D page entry

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,5 +7,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("./index.html", import.meta.url)),
+        threeD: fileURLToPath(new URL("./3d/index.html", import.meta.url)),
+      },
+    },
   },
 });
